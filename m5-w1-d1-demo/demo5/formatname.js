cdfcae2128377e86552e7f69f75dc4941f9fc24e
Yinghai Yu <Y15653862659@outlook.com>
2023-01-03 19:54:50 -0800

    done

--- m5-w1-d1-demo/demo5/formatname.js
+++ m5-w1-d1-demo/demo5/formatname.js
@@ -1,5 +1,13 @@
 //add function here
-  
+  function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+  }
+
+  const user = {
+    firstName: 'West',
+    lastName: 'Cliff',
+  }
+
   const element = <h1>Hello, {formatName(user)}!</h1>;
   
   ReactDOM.render(element, document.getElementById('root'));
