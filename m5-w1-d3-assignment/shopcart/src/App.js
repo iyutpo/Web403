a7fe2abc71af25b774cc2bf53fc133195d1a4654
Yinghai Yu <Y15653862659@outlook.com>
2023-01-07 12:31:02 -0800

    m1d3assignment

--- m5-w1-d3-assignment/shopcart/src/App.js
+++ m5-w1-d3-assignment/shopcart/src/App.js
@@ -5,23 +5,47 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { faShoppingCart} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
+import cologne from './m5-assignment/products/cologne.jpg';
+import iwatch from './m5-assignment/products/iwatch.jpg';
+import mug from './m5-assignment/products/mug.jpg';
+import wallet from './m5-assignment/products/wallet.jpg';
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Shop to React
+          <FontAwesomeIcon icon={faShoppingCart} />
+          0 items
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+
+      <ul class="list-group">
+        <li class="list-group-item d-flex align-items-start">
+          Unisex Cologne <br></br>
+          <img src={cologne} alt="" />
+          <input placeholder='0'></input> quantity
+        </li>
+        <li class="list-group-item d-flex align-items-start">
+          Apple iWatch <br></br>
+          <img src={iwatch}></img>
+          <input placeholder='0'></input> quantity
+        </li>
+        <li class="list-group-item d-flex align-items-start">
+          Unique Mug <br></br>
+          <img src={mug}></img>
+          <input placeholder='0'></input> quantity
+        </li>
+        <li class="list-group-item d-flex align-items-start">
+          Mens Wallet <br></br>
+          <img src={wallet}></img>
+          <input placeholder='0'></input> quantity
+        </li>
+      </ul>
+
     </div>
   );
 }
