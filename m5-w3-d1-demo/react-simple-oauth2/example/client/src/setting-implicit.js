8306c1b9f0edef6f0e99e61e750b39cb57283ad6
Yinghai Yu <Y15653862659@outlook.com>
2023-01-08 15:52:55 -0800

    m5-w3-d1-demo

---
+++ m5-w3-d1-demo/react-simple-oauth2/example/client/src/setting-implicit.js
@@ -0,0 +1,21 @@
+// Ideally you should take these vars from process.env.REACT_APP_*
+// and define them in e.g. .env.local (if you're using Create React App)
+
+// Authorization screen base URL
+// e.g. https://developer.github.com/apps/building-oauth-apps/authorizing-oauth-apps/#1-request-a-users-github-identity
+export const authorizationUrl = 'https://github.com/login/oauth/authorize';
+// https://docs.github.com/en/developers/apps/authorizing/authorizing-oauth-apps
+
+// To get a client ID, create an app, e.g.
+// GitHub (authorization code grant only): https://github.com/settings/developers
+// Spotify (implicit grant & auth code): https://developer.spotify.com/dashboard/applications
+export const clientId = '77b71d22c8fbb0f123bc';
+
+// You get to configure this in your OAuth settings
+// If you use React Router, the relative path (empty here) can match
+// that of a route which displays nothing
+export const redirectUri = 'http://localhost:1234/';
+
+// Authorization code flow only: base URL for your server
+// The one provided below is that of the sample Express server provided
+export const serverUrl = 'http://localhost:5000';
