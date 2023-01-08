58c07dcf64131e663f036bdeb139e83a24b76ce7
Yinghai Yu <Y15653862659@outlook.com>
2023-01-08 11:46:01 -0800

    m5-w2-d1-demo

--- m5-w2-d1-demo/demo3.js
+++ m5-w2-d1-demo/demo3.js
@@ -1,11 +1,13 @@
   class NameForm extends React.Component {
     constructor(props) {
       super(props);
-      
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.input = React.createRef();
     }
   
     handleSubmit(event) {
-      
+      alert('A name was submitted: ' + this.input.current.value);
+      event.preventDefault();
     }
   
     render() {
@@ -13,7 +15,7 @@
         <form onSubmit={this.handleSubmit}>
           <label>
             Name:
-            <input type="text"  />
+            <input type="text" ref={this.input} />
           </label>
           <input type="submit" value="Submit" />
         </form>
