f9111ed16917ea13ea49935495cbd2b9036a8981
yyu441 <yyu441@gatech.edu>
2023-01-25 10:08:49 -0800

    m5-w4-d1-homework

---
+++ m5-w4-d1-homework/react-weather-app/src/App.js
@@ -0,0 +1,83 @@
+import './App.css';
+import countries from 'i18n-iso-countries';
+import { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-font-awesome';
+import { faTerperatureLow, faTemperatureHigh, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+
+
+
+countries.registerLocale(require('i18n-iso-countries/langs/en.json'));
+
+
+function App() {
+  // State
+  const [apiData, setApiData] = useState({});
+  const [getState, setGetState] = useState('Irvine, USA');
+  const [state, setState] = useState('Irvine, USA');
+
+  // API key and url
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${state}&appid=${apiKey}`;
+
+  // side effect
+  useEffect(() => {
+    fetch(apiUrl)
+    .then((res) => res.json())
+    .then((data) => setApiData(data));
+  }, [apiUrl]);
+
+  const inputHandler = (event) => {
+    setGetState(event.target.value);
+  };
+
+  const submitHandler = () => {
+    setState(getState);
+  };
+
+  const kelvinToFarenheit = (k) => {
+    return ((k - 273.15) * 1.8 + 32).toFixed(0);
+  };
+
+  return (
+    <div className='App'>
+      <header className='d-flex justify-content-center align-items-center'>
+        <h2>React Weather App</h2>
+      </header>
+      <div className='container'>
+        <div className='mt-3 d-flex flex-column justify-content-center align-items-center'>
+          <div class='col-auto'>
+            <label for='location-name' class='col-form-label'>
+              Enter Location:
+            </label>
+          </div>
+
+          <div class='col-auto'>
+            <input 
+              type='text'
+              id='location-name'
+              class='form-control'
+              onChange={inputHandler}
+              value={getState}
+            />
+          </div>
+          <button className='btn btn-primary mt-2' onClick={submitHandler}>
+            Search
+          </button>
+        </div>
+
+        <div className='card mt-3 max-auto'>
+          {/* Is it true data coming in from open weather based on input location */}
+          {apiData.main
+          ? (<div class='card-body text-center'></div>)  /* Load data if true */
+          : (<h1>Locading....</h1>)}   {/* Output message if false */}
+        </div>
+      </div>
+      <footer className='footer'>
+        &copy; React Weather App
+      </footer>
+    </div>
+  );
+}
+
+export default App;
