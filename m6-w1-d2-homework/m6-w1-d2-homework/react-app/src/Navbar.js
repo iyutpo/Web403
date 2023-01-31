7bf54ee715b69fdcdeaf552d765d9ddc5143d040
yyu441 <yyu441@gatech.edu>
2023-01-31 12:09:30 -0800

    m6-w1-d2-homework

---
+++ m6-w1-d2-homework/m6-w1-d2-homework/react-app/src/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {Navbar, NavbarBrand} from 'reactstrap';
+import {Link} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default class AppNavbar extends Component {
+
+    render() {
+        return (
+            <Navbar className='navbar-dark bg-dark px-5' expand='md'>
+                <NavbarBrand tag={Link} to='/'>Home</NavbarBrand>
+            </Navbar>
+        )
+    }
+}
